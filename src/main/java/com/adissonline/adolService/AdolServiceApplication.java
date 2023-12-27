832d12df4dfbffe5b087da8575fb0938d918ae0b
Fereja <fereja@live.com>
2023-12-26 19:20:50 -0800

    Initial commit

---
+++ src/main/java/com/adissonline/adolService/AdolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adissonline.adolService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdolServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adissonline/adolService/AdolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adissonline.adolService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
